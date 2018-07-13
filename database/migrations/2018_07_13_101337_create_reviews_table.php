eead9f72ac872fab57eef70a30fc0771b4eae51d
Miklós Majtényi <miklosmajtenyi@Mikloss-MacBook-Pro.local>
2018-07-13 19:09:21 +0200

    Migration created for product and reviews

--- database/migrations/2018_07_13_101337_create_reviews_table.php
+++ database/migrations/2018_07_13_101337_create_reviews_table.php
@@ -15,6 +15,15 @@ class CreateReviewsTable extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('product_id')->unsigned()
+                                                 ->index();
+
+            $table->foreign('product_id')->references('id')
+                                                  ->on('product_id')
+                                                  ->onDelete('cascade');
+            $table->string('customer');
+            $table->text('review');
+            $table->integer('star');
             $table->timestamps();
         });
     }
