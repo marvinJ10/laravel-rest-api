a8d635e4de4a9ece6470812d0cd80b2d0b7b0c38
Miklós Majtényi <miklosmajtenyi@Mikloss-MacBook-Pro.local>
2018-07-23 17:33:37 +0200

    Add basic exception handlers

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,7 +3,10 @@
 namespace App\Exceptions;
 
 use Exception;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -46,6 +49,20 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if($request->expectsJson()){
+            if($exception instanceof ModelNotFoundException){
+                return response([
+                    'error' => 'Product model not found'
+                ], Response::HTTP_NOT_FOUND);
+            }
+
+            if($exception instanceof NotFoundHttpException){
+                return response([
+                    'error' => 'Incorrect route'
+                ], Response::HTTP_NOT_FOUND);
+            }
+        }
+
         return parent::render($request, $exception);
     }
 }
